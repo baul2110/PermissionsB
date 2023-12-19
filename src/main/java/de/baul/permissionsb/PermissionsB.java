4301b3181ec6d6cc7eb70ae1160d7d26d4c1d3f8
BuildTools <unconfigured@null.spigotmc.org>
2023-12-19 23:17:24 +0100

    Initial Commit

---
+++ src/main/java/de/baul/permissionsb/PermissionsB.java
@@ -0,0 +1,17 @@
+package de.baul.permissionsb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PermissionsB extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
